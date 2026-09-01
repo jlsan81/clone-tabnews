bada6dfb2a4bfb05e7fd758836422e5b4241fb00
jlsan81 <56101793+jlsan81@users.noreply.github.com>
2026-09-01 17:56:31 +0000

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1+1 as sum;');
+  console.log(result.rows);
   response
     .status(200)
-    .json({ "Sempre Flamengo": "Vai pra cima deles Mengão!" });
+    .json({"Sempre Flamengo": "Vai pra cima deles Mengão!" });
 }
 
-export default status; 
+export default status;  
